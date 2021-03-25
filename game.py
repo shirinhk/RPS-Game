2be730f6bb9c42dda85c3ec5fb31c41b2b769465
Shirin Hk <shirinhk@Shirins-MacBook-Pro.local>
2021-03-25 15:24:34 -0400

    update game file

--- game.py
+++ game.py
@@ -6,12 +6,12 @@ while gameVars.player_choice is False:
     print("-_-_-_-_-_-_-_-_-_-_ FUN RPS GAME -_-_-_-_-_-_-_-_-_-_")
     print("Computer Lives:", gameVars.computer_lives, "/", gameVars.total_lives)
     print("Player Lives:", gameVars.player_lives, "/", gameVars.total_lives)
-    print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- ")
+    print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
     print("Choose your weapon! Or type quit to exit\n") #\n means "new line"
     gameVars.player_choice = input("Choose rock, paper, or scissors: \n")
 
     if gameVars.player_choice == "quit":
-        print("-_-_-_-_-_-_-_-_ You chose to quit -_-_-_-_-_-_-_-")
+        print("-_-_-_-_-_-_-_-_-_ You chose to quit -_-_-_-_-_-_-_-_-")
         exit()
 
     gameVars.computer_choice = gameVars.choices[randint(0, 2)]
@@ -20,7 +20,7 @@ while gameVars.player_choice is False:
     print("computer chose: " + gameVars.computer_choice)
 
     if gameVars.computer_choice == gameVars.player_choice:
-        print("-_-_-_-_-_-_-_-_-_-_ tie -_-_-_-_-_-_-_-_-_-_ ")
+        print("-_-_-_-_-_-_-_-_-_-_-_- tie -_-_-_-_-_-_-_-_-_-_-_-_-_ ")
     elif gameVars.computer_choice == "rock":
         if gameVars.player_choice == "scissors":
             gameVars.player_lives -= 1
